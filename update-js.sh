00ce11e7d2501854f42372833232b68e6a8175d0
Linus Jahn <lnj@kaidan.im>
2019-10-02 20:28:28 +0200

    Add quality-selector to player

---
+++ update-js.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# videojs quality-selector: https://github.com/silvermine/videojs-quality-selector
+curl -L https://unpkg.com/silvermine-videojs-quality-selector/dist/js/silvermine-videojs-quality-selector.min.js > assets/js/videojs.quality-selector.min.js
+curl -L https://unpkg.com/silvermine-videojs-quality-selector/dist/css/quality-selector.css > assets/css/videojs.quality-selector.css
